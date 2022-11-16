47d53abc1fd639f6b2cc58ba203c9ba411d3c3e9
Vian66 <vicky926better801637@gmail.com>
2022-11-16 17:12:43 +0800

    [insert] 客製input/file css

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
-import './assets/main.css'
+import './assets/styles/all.css'
 
 const app = createApp(App)
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,40 @@
+<template>
+  <span class="appName">閃簽・就是快</span>
+
+  <div class="upLoadgroup">
+    <img src="../../public/img/blockupload.png" alt />
+    <div class="upLoaditem">
+      <input ref="input" type="file" id="uploader" @change="handleFiles" style="opacity: 0;" />
+    </div>
+  </div>
+</template>
 <script setup>
 </script>
+<style lang="scss" scoped>
+.upLoadgroup {
+  float: right;
+  width: 300px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  img {
+    position: relative;
+  }
+  .upLoaditem {
+    position: absolute;
+    width: 82px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    input {
+      opacity: 0;
+      position: absolute;
+      height: 182px;
+    }
+  }
+}
+</style>
 
-<template>
-  <main>
-    <h1>helloHome</h1>
-    <button type="button" class="btn btn-primary">Primary</button>
-  </main>
-</template>
+
+
