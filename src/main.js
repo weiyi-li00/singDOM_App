25ee16bb81db7215733c5f80f991a03ab9788de8
Vian66 <vicky926better801637@gmail.com>
2022-11-17 14:30:29 +0800

    [insert] 現PDF.js並render所有頁面

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,40 +1,20 @@
 <template>
-  <span class="appName">閃簽・就是快</span>
-
   <div class="upLoadgroup">
     <img src="../../public/img/blockupload.png" alt />
     <div class="upLoaditem">
       <input ref="input" type="file" id="uploader" />
     </div>
   </div>
-  <canvas id="canvas" style="border: 1px solid #000"></canvas>
+  <div id="pdfContainer" class="pdfContainer"></div>
 </template>
 <script setup>
-pdfjsLib.GlobalWorkerOptions.workerSrc =
-  "https://mozilla.github.io/pdf.js/build/pdf.worker.js";
-
-console.log(pdfjsLib);
-pdfjsLib.getDocument("./sample.pdf").promise.then((doc) => {
-  console.log(doc._pdfInfo.numPages);
-  // 抓取第一頁
-  doc.getPage(doc._pdfInfo.numPages).then((page) => {
-    console.log(page);
-    // 設定 PDF 內容的顯示比例
-    const viewport = page.getViewport({ scale: 1 });
-
-    // 設定 canvas 的大小與 PDF 相等
-    canvas.width = viewport.width;
-    canvas.height = viewport.height;
-
-    //實際渲染 PDF
-    page.render({
-      canvasContext: ctx,
-      viewport: viewport,
-    });
-  });
-});
+import { ref, onMounted } from "vue";
 </script>
 <style lang="scss" scoped>
+.pdfContainer {
+  width: 870px;
+  margin: 0 auto;
+}
 .upLoadgroup {
   float: right;
   width: 300px;
