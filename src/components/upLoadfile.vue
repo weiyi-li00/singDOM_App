2cca9bd271955c63e0ded78e44f2e7abfd3a5c87
Vian66 <vicky926better801637@gmail.com>
2022-11-22 18:13:55 +0800

    [add] upLoadfile

---
+++ src/components/upLoadfile.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+<template>
+  <div class="upLoaddata"></div>
+  <div class="showData"></div>
+</template>
+<style lang="scss" scoped>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { ref, onMounted } from "vue";
+import upLoadfile from "../components/upLoadfile.vue";
 </script>
-<template></template>
+<template>
+  <upLoadfile></upLoadfile>
+</template>
 
 <style lang="scss" scoped></style>
