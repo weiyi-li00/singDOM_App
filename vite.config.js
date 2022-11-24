7ff052ae249884e3a4cef2cfdcd019d1bdda7827
Vian66 <vicky926better801637@gmail.com>
2022-11-24 16:06:25 +0800

    [update] vit.config

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,27 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import ElementPlus from 'unplugin-element-plus/vite'
+import {
+  ElementPlusResolver,
+  AntDesignVueResolver,
+  VantResolver,
+  HeadlessUiResolver,
+  ElementUiResolver
+} from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     Components({
-      resolvers: [ElementPlusResolver()]
+      resolvers: [
+        ElementPlusResolver(),
+        AntDesignVueResolver(),
+        VantResolver(),
+        HeadlessUiResolver(),
+        ElementUiResolver()
+      ]
     })
   ],
   resolve: {
